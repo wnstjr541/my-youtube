252fe5c844b99b3f427d80f94acaa278d42d5b6c
wnstjr541 <wnstjr541@naver.com>
2022-08-23 14:16:21 +0900

    git 배포

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './app';
+// import Youtube from './service/youtube';
+
+// const youtube = new Youtube(process.env.REACT_APP_TUBE_API_KEY);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App youtube={youtube} />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
